19c9d17672a9951fcb6b1099a01aaa07d1438c8c
Ofek Ashery <ofek.ashery@gmail.com>
2019-11-29 17:26:07 +0200

    Rewrite prompts

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const readline = require('readline');
+const prompts = require('prompts');
 const nodeFetch = require('node-fetch');
-const fetch = require('fetch-cookie')(nodeFetch);
+const fetch = require('fetch-cookie/node-fetch')(nodeFetch);
 const WebSocket = require('ws');
 let host, event, overlay;
 let matchTimer;
-
 
 login().then(() => {
   console.log('\nConnected!');
@@ -33,32 +32,54 @@ login().then(() => {
 });
 
 async function login() {
-  host = await getInput('Enter the scorekeeper IP address:');
-  event = await getInput('Enter the event code:');
-  const user = await getInput('Enter username:');
-  const pass = await getInput('Enter password:');
-  const overlayInput = (await getInput('Do you use overlay? (yes/no)')).toLowerCase();
-  overlay = overlayInput === 'y' || overlayInput === 'yes';
-  return fetch(`http://${host}/callback/`, {
-    'headers': {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    'redirect': 'manual',
-    'body': `username=${user}&password=${pass}&submit=Login&client_name=FormClient`,
-    'method': 'POST'
-  }).then((res) => {
-    if (res.url.includes('CredentialsException')) throw Error('Incorrect username or password.');
-  });
-}
-
-function getInput(title) {
-  return new Promise((resolve, reject) => {
-    process.stdout.write(title + ' ');
-    const input = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      terminal: false
+  host = (await prompts({
+    type: 'text',
+    name: 'host',
+    initial: 'localhost',
+    message: 'What is your Scorekeeper IP?'
+  })).host;
+  return fetch(`http://${host}/api/v1/events/`).then(res => res.json()).then(async (events) => {
+    const names = {};
+    for (const code of events.eventCodes) {
+      names[code] = await fetch(`http://${host}/api/v1/events/${code}/`).then(res => res.json()).then(res => res.name);
+    }
+    const response = await prompts([
+      {
+        type: 'text',
+        name: 'username',
+        initial: 'local',
+        message: 'What is your Scorekeeper username?'
+      },
+      {
+        type: 'text',
+        name: 'password',
+        message: 'What is your Scorekeeper password?'
+      },
+      {
+        type: 'select',
+        name: 'event',
+        message: 'Select your event',
+        choices: Object.keys(names).map((code) => ({ title: names[code], description: code}))
+      },
+      {
+        type: 'confirm',
+        name: 'overlay',
+        message: 'Do you use overlay?'
+      }
+    ]);
+    event = events.eventCodes[response.event];
+    overlay = response.overlay;
+    return fetch(`http://${host}/callback/`, {
+      'headers': {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      'redirect': 'manual',
+      'body': `username=${response.username}&password=${response.password}&submit=Login&client_name=FormClient`,
+      'method': 'POST'
+    }).then((res) => {
+      if (res.headers.get('Location').includes('CredentialsException')) throw 'Incorrect username or password.';
     });
-    input.on('line', resolve);
-  });
+  }).catch((e) => {
+    throw Error(typeof e === 'string' ? e : 'Cannot access the scorekeeper.');
+  })
 }
